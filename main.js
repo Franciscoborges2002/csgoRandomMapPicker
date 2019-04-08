dfc0398f0fb0e108e9d4cdd305c679dd17482f48
Francisco Borges <30115559+V0X4R1N0@users.noreply.github.com>
2019-04-08 21:48:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -18,4 +18,10 @@ function randomMap(){
   var randomItem = maps[Math.floor(Math.random()*maps.length)];
 
   document.getElementById('result').innerHTML = randomItem;
+    
+  for(var i = 0; i>0; i++){
+    document.getElementById('history').innerHTML = randomItem;
+}    
+
+    
 };
